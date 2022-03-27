46c29627439770508eb0a7159de1397c5f242104
Marta Collu <marta.collu93@gmail.com>
2022-03-27 22:52:46 +0200

    catchError e Retry

--- ngprj/src/app/project/project.service.ts
+++ ngprj/src/app/project/project.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Project } from '@app/models/Project';
 import { LogService } from '@app/shared/log.service';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,9 @@ export class ProjectService {
   getAll(): Observable<Project[]> {
     return this.httpClient.get<Project[]>('http://localhost:3000/projects')
     .pipe(
-      tap((data) => this.logService.log(`getAll Eseguito ${data}`))
+      tap((data) => this.logService.log(`getAll Eseguito ${data}`)),
+      retry(3),
+      catchError(this.handleError)
     );
   }
 
@@ -29,7 +31,9 @@ export class ProjectService {
 
     return this.httpClient.post<Project>('http://localhost:3000/projects', projectToAdd)
     .pipe(
-      tap((data) => this.logService.log(`add Eseguito ${data}`))
+      tap((data) => this.logService.log(`add Eseguito ${data}`)),
+      retry(3),
+      catchError(this.handleError)
     );
 
   }
@@ -37,7 +41,23 @@ export class ProjectService {
   get(id: number): Observable<Project> {
     return this.httpClient.get<Project>(`http://localhost:3000/projects/${id}`)
     .pipe(
-      tap((data) => this.logService.log(`get Eseguito ${data}`))
+      tap((data) => this.logService.log(`get Eseguito ${data}`)),
+      retry(3),
+      catchError(this.handleError)
     );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`
+      );
+    }
+    return throwError(
+      'Something bad happened; please try again later.'
+    );
+  };
 }
