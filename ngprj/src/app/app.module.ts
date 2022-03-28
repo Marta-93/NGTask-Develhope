521126fa086470042a5b504250898e43bb03a50e
Marta Collu <marta.collu93@gmail.com>
2022-03-28 21:40:01 +0200

    spostamento componenti

--- ngprj/src/app/app.module.ts
+++ ngprj/src/app/app.module.ts
@@ -1,33 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { ProjectDashboardComponent } from './modules/project/pages/project-dashboard/project-dashboard.component';
-import { ProjectDetailComponent } from './modules/project/pages/project-detail/project-detail.component';
-import { ProjectListComponent } from './modules/project/components/project-list/project-list.component';
-import { ProjectFormComponent } from './modules/project/components/project-form/project-form.component';
 import { HomeComponent } from './modules/dashboard/pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-import { SectionHeaderComponent } from './shared/section-header/section-header.component';
+import { ProjectModule } from './modules/project/project.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectDashboardComponent,
-    ProjectDetailComponent,
-    ProjectListComponent,
-    ProjectFormComponent,
     HomeComponent,
-    NavbarComponent,
-    SectionHeaderComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    ProjectModule,
     AppRoutingModule
   ],
   providers: [],
