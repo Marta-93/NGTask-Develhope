10c80c2812f3891ac7181f2851427da1faa18d6b
Marta Collu <marta.collu93@gmail.com>
2022-03-28 17:07:21 +0200

    modified: organizzazione dell'app

---
+++ ngprj/src/app/modules/project/pages/project-detail/project-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Project } from '@app/models/Project';
+import { Observable, switchMap } from 'rxjs';
+import { ProjectService } from '../../project.service';
+
+@Component({
+  selector: 'ngprj-project-detail',
+  templateUrl: './project-detail.component.html',
+  styleUrls: ['./project-detail.component.css']
+})
+export class ProjectDetailComponent implements OnInit {
+  project$!: Observable<Project>;
+
+  constructor(
+    private projectService: ProjectService,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    this.project$ = this.activatedRoute.paramMap.pipe(
+      switchMap((params: ParamMap) => this.projectService.get(+params.get('id')!))
+    )
+  }
+
+}
